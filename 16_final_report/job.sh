2fe7b185b0a9bc46a7c7c550cfc9c279d7db32ed
HosokiRyubu <hosoki.r.aa@m.titech.ac.jp>
2021-12-06 16:51:08 +0900

    add file which not git_add

--- 16_final_report/job.sh
+++ 16_final_report/job.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #$ -cwd
-#$ -l f_node=8
+#$ -l f_node=2
 #$ -l h_rt=00:10:00
-#$ -N node_8
+#$ -N node_2
 
 . /etc/profile.d/modules.sh
 module load gcc/8.3.0 cuda openmpi
 
-#mpirun -n 8 -npernode 1 --bind-to none ./a.out
-mpirun -n 8 -npernode 1 --bind-to none -x OMP_NUM_THREADS=56 ./a.out
+#mpirun -n 2 -npernode 1 --bind-to none ./a.out
+mpirun -n 2 -npernode 1 --bind-to none -x OMP_NUM_THREADS=56 ./a.out
